eb25a1d19881c5ef7d0d442b829d632a0dfac6db
khairiyajava <kombokhairiya@gmail.com>
2023-05-05 05:49:12 +0300

    myweb

---
+++ login.js
@@ -0,0 +1,17 @@
+function login() {
+	let username = document.getElementById("username").value;
+	let password = document.getElementById("password").value;
+
+	if (username.trim() == "" || password.trim() == "") {
+		alert("Please enter your username and password.");
+	} else {
+		localStorage.setItem("username", username);
+		localStorage.setItem("password", password);
+		alert("Successfully.");
+	}
+}
+
+function clearForm() {
+	document.getElementById("username").value = "";
+	document.getElementById("password").value = "";
+}
